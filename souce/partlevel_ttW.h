76d4ed1177a4f3e0ba1fc061dcb58815a23d46f1
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2019-08-05 15:42:26 +0200

    remove default message with comments

--- souce/partlevel_ttW.h
+++ souce/partlevel_ttW.h
@@ -105,23 +105,12 @@ public :
 #ifdef partlevel_ttW_cxx
 void partlevel_ttW::Init(TTree *tree)
 {
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the reader is initialized.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
 
    fReader.SetTree(tree);
 }
 
 Bool_t partlevel_ttW::Notify()
 {
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
 
    return kTRUE;
 }

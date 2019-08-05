c736d77315a7c08c294387c487108b24309c58c2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2019-08-05 14:50:12 +0200

    steering macro for tselector

---
+++ run.C
@@ -0,0 +1,17 @@
+void run(string name="Sherpa", string comp="a"){
+  TChain *ch=new TChain("particleLevel");
+  string choose_tag="";
+  string name1="";
+  if(name.find("Sherpa")!= std::string::npos) name1="413008"; 
+  else if(name.find("MG")!= std::string::npos) name1="410155";
+ 
+  string path="/Users/grevtsov/Documents/working_files/ttH/ttH-ML/Combination_ttV/Files/particle_level/"+name1+"d.root";
+  cout << "path = " << path<< endl;
+
+  ch->Add(path.c_str());
+  cout << ch->GetNtrees()<< ", entr: "<< ch->GetEntries()<< endl;
+  //eventually, start Proof Lite on cores
+  //TProof::Open("workers=4");
+  //ch->SetProof();
+  ch->Process("souce/partlevel_ttW.C+",name.c_str());
+}

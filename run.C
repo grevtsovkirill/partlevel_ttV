76fcdd18269c59b2e43f128a04913c3ad51edd54
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2019-08-09 10:29:10 +0200

    update to new version of ntuples with relaxed lepton multiplicity selections

--- run.C
+++ run.C
@@ -5,7 +5,7 @@ void run(string name="Sherpa", string comp="a"){
   if(name.find("Sherpa")!= std::string::npos) name1="413008"; 
   else if(name.find("MG")!= std::string::npos) name1="410155";
  
-  string path="/Users/grevtsov/Documents/working_files/ttH/ttH-ML/Combination_ttV/Files/particle_level/"+name1+"d.root";
+  string path="/Users/grevtsov/Documents/working_files/ttH/ttH-ML/Combination_ttV/Files/particle_level/"+name1+"d_v3.root";
   cout << "path = " << path<< endl;
 
   ch->Add(path.c_str());

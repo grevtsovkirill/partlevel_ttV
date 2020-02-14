a578f61bc05c04207da3f816c7359897dd133338
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-14 09:16:28 +0100

    update output files

--- qqw.py
+++ qqw.py
@@ -1,9 +1,14 @@
 import os
 
-#samples = ['413008_ttw','410472_ttbar','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau']
-samples = ['410220_tttautau']
+samples = ['413008_ttw','410472_ttbar','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau']
+#suff = "norm"
+suff = "xs"
+#samples = ['410220_tttautau']
 for i in samples:
     tick = "'"
-    command = 'root -l -b -q '+tick+'QQrun.C("'+i+'")'+tick
+    option = ""
+    if suff:
+        option = '","'+suff
+    command = 'root -l -b -q '+tick+'QQrun.C("'+i+option+'")'+tick
     print(command)
     os.system(command)

cef628f72930dfe170294ca3ab3ea032d8fe5b07
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-28 14:21:32 +0100

    move to new samples including truth information of the origin of leptons

--- QQrun.C
+++ QQrun.C
@@ -17,7 +17,7 @@ void QQrun(string name="413008_ttw", string comp="xs"){
   //if(name.find("Sherpa")!= std::string::npos) name1="413008_ttw"; 
   //else if(name.find("MG")!= std::string::npos) name1="410155";
 
-  string path="/Users/grevtsov/Documents/working_files/ttH/Wtt_run2/Files/ttWqq/v0_0206/"+name+".root";;
+  string path="/Users/grevtsov/Documents/working_files/ttH/Wtt_run2/Files/ttWqq/v1_0228/"+name+".root";;
   cout << "path = " << path<< endl;
 
 

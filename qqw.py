6220dd3599597ff5e2d29878f0b59e5a5a98fc30
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-28 16:54:01 +0100

    create distributions of the type/origin for leptons

--- qqw.py
+++ qqw.py
@@ -1,7 +1,7 @@
 import os
 
-samples = ['413008_ttw','410472_ttbar','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau']
-#suff = "norm"
+samples = ['413008_ttw','410155_ttw','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau']
+#suff = "norm" ,'410472_ttbar'
 suff = "xs"
 #samples = ['410220_tttautau']
 for i in samples:

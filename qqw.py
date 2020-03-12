ab75d74b621fc7733e006815769d8fe0f80ec94c
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-12 12:53:36 +0100

    extend loop to additional samples

--- qqw.py
+++ qqw.py
@@ -1,6 +1,7 @@
 import os
 
-samples = ['413008_ttw','410155_ttw','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau','410472_ttbar']
+samples = ['413008_ttw','410155_ttw','410156_ttZnunu','410157_ttZqq','410218_ttee','410219_ttmumu','410220_tttautau','410472_ttbar',
+           '410658_topt','410659_atopt','410644_tops','410645_atops','304014_threeTop','410080_fourTop','410081_ttww','410408_WtZ']
 #suff = "norm" ,'410472_ttbar'
 suff = "xs"
 #samples = ['410220_tttautau']

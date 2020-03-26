2bb30955a4d5b25344f6e891527a54a4e5cb9665
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-26 16:52:36 +0100

    propagate multi reader option to recoLev. resolves #2

--- recoQQ.C
+++ recoQQ.C
@@ -1,24 +1,36 @@
-void recoQQ(string name="413008", string comp="xs", bool onNAF = true){
+void recoQQ(string name="413008", string comp="xs", bool onNAF = true, bool multi_file = false, int num_split =4){
   /*
     413008.root
   */
 
   TChain *ch=new TChain("nominal");
-  string choose_tag="";
-  string name1="";
+  string choose_tag="d";
+  string tmp="";
   string path="";
   string prod_version = "GFW1_v0_0320"; //
   if(onNAF)
-    path="/nfs/dust/atlas/group/top/ttH_ML/ttW_run2/ttWqq/Reco_files/"+prod_version+"/mc16d/"+name+".root";  
+    path="/nfs/dust/atlas/group/top/ttH_ML/ttW_run2/ttWqq/Reco_files/"+prod_version+"/mc16"+choose_tag+"/";  
   else
-    path="/Users/grevtsov/Documents/working_files/ttH/Wtt_run2/Files/ttWqq/Reco_files/"+prod_version+"/mc16d/"+name+".root";
+    path="/Users/grevtsov/Documents/working_files/ttH/Wtt_run2/Files/ttWqq/Reco_files/"+prod_version+"/mc16"+choose_tag+"/";
 
   cout << "path = " << path<< endl;
+
+  if(!multi_file){
+    path = path+name+".root";
+  }
 
 
 
   TChain *weight_chain=new TChain("sumWeights");
-  weight_chain->Add(path.c_str());
+  if(!multi_file){
+    weight_chain->Add(path.c_str());
+  }
+  else{
+    for(int i=0; i<num_split;i++){      
+      tmp = path+name+"_"+to_string(i)+".root";
+      weight_chain->Add(tmp.c_str());
+    }
+  }
   Double_t  sum_w=0;
   Float_t totalEventsWeighted;
   weight_chain->SetBranchAddress("totalEventsWeighted",&totalEventsWeighted);
@@ -29,7 +41,16 @@ void recoQQ(string name="413008", string comp="xs", bool onNAF = true){
   }
   cout<<" total sw:"<<to_string(sum_w)<<endl;
 
-  ch->Add(path.c_str());
+  if(!multi_file){
+    ch->Add(path.c_str());
+  }
+  else{
+    for(int i=0; i<num_split;i++){      
+      tmp = path+name+"_"+to_string(i)+".root";
+      ch->Add(tmp.c_str());
+    }
+  }
+
   cout << ch->GetNtrees()<< ", entr: "<< ch->GetEntries()<< endl;
   string  option=to_string(sum_w)+"/"+name+"-"+comp;
 

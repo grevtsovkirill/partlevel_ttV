3d41863adedd69baf89f25d17445e1769e0348a2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-06-06 16:08:56 +0200

    define jet counter

--- ctag/definitions.py
+++ ctag/definitions.py
@@ -2,3 +2,14 @@ getDL1function= '''
 using namespace ROOT::VecOps;
 
 '''
+
+countJets= '''
+int CountJets(RVec<float> DL1c,RVec<float> DL1b, float cut) {
+    int njets = 0;
+    for(unsigned int i = 0 ; i < DL1c.size() ; ++i){
+         if(DL1c[i]>=cut && DL1b[i]<2.195)
+            ++njets;
+    }
+    return njets;
+}
+'''

c31d3f0ccf41692729c2fe15279eb694c6311193
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-06-06 16:02:05 +0200

    load files

--- ctag/ctag_opt.py
+++ ctag/ctag_opt.py
@@ -3,3 +3,24 @@ import ROOT
 import array
 
 from  definitions import *
+
+
+ROOT.gInterpreter.Declare(getDL1function+countJets)
+
+
+def main():
+    ntupleLoc = 'Files/'
+
+    bkgFiles = ["ctag_410470_d"]
+    bkgFilesToUse=ROOT.vector("string")()
+    for infile in bkgFiles:
+        fn = ntupleLoc+infile+'.root'
+        inFile=ROOT.TFile(fn,'r')
+        if inFile.IsZombie() or not inFile.Get("ftagTree"):
+            bkgFiles.remove(infile)
+            inFile.Close()
+            continue
+        bkgFilesToUse.push_back(infile)
+
+if __name__== "__main__":
+    main()

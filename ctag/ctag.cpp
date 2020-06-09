f09171169fdd6220f6151126f9b7b0f74cd78663
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-06-09 17:34:11 +0200

    tagger optimization macro - get efficiency to fb and rejections estimate. resolves #24

--- ctag/ctag.cpp
+++ ctag/ctag.cpp
@@ -52,7 +52,8 @@ void ctag(){
   gROOT->Reset();
   SetAtlasStyle();
   
-  TFile *f = TFile::Open("Files/ctag_700000_d.root");
+  //TFile *f = TFile::Open("Files/ctag_700000_d.root");
+  TFile *f = TFile::Open("Files/ctag_410470_d.root");
   TTree *t1 = (TTree*)f->Get("ftagTree");
 
   double weight_tot;
@@ -77,22 +78,22 @@ void ctag(){
   float fc=0.018;
   float fb=0.28;
   float wp70t=3.245;
-  TH1D *hist_dlrc[10][10];
+  TH1D *hist_dlrc[20][20];
   
-  vector<float> fbs = {0.28,0.02,0.03};
+  vector<float> fbs = {0.01,0.1,0.3,0.5,0.7};
   vector<int> flav = {0,4,5};
   vector<string> typeflav = {"l","c","b"};
   Int_t color_sample[12]={416,632,600};
-  vector<float> dl1r_b[10];
-  vector<float> dl1r_c[10];
-  vector<float> dl1r_c_part[10][10];
-  vector<float> w_vec[10];
+  vector<float> dl1r_b[20];
+  vector<float> dl1r_c[20];
+  vector<float> dl1r_c_part[20][20];
+  vector<float> w_vec[20];
   for(int i=0; i<(int)fbs.size();i++){
     for(int k=0; k<(int)flav.size();k++){
       hist_dlrc[i][k] = new TH1D(("dlrc_"+typeflav[k]+"_"+to_string(i)).c_str(), ("DL1r_{c} "+typeflav[k]+" "+to_string(fbs[i])+";DL1r_{c};Events").c_str(), 75, -10, 15);
     }
   }
-  hist_dlrc[0][9] = (TH1D*)hist_dlrc[0][0]->Clone();
+  //hist_dlrc[0][9] = (TH1D*)hist_dlrc[0][0]->Clone();
   
   for(int i=0; i<t1->GetEntries(); i++){
     t1->GetEntry(i);
@@ -120,40 +121,69 @@ void ctag(){
   }//evet loop
 
 
-  // for(int i=0; i<dl1r_c[0].size(); i++){
-  //   //hist_dlrc[0][9]->Fill(dl1r_cc[0].at(i),w_vec[0].at(i));
-    
-  // }  
+  cout << "finish event loop "<< endl;
   TCanvas * canv[100];
   TLegend* legend[100];
-  char canvas_name[1000]; char text1[1000]; char text2[1000]; 
+  char canvas_name[1000]; char text1[1000]; char text2[1000];char text3[1000]; char o_name[1000];
+  TLine *ll[10];
   TString atl_lable = "Internal";
   TLatex latex2; latex2.SetTextSize(0.045); latex2.SetNDC();
-  for(int i=0; i<1;i++){
+  Float_t cut_val = 0;
+  Float_t target_c_efficiency = 0.5;
+  vector<float> b_rejection ;
+  vector<float> l_rejection ;
+  float br = 0;  float lr = 0;
+  float tmp = 0;
+  for(int i=0; i<(int)fbs.size();i++){
+  //for(int i=0; i<1;i++){
+    cut_val = getCut(hist_dlrc[i][1],target_c_efficiency);
+    tmp = CountJets(dl1r_c_part[i][2] ,dl1r_b[0], -10);
+    br = tmp /CountJets(dl1r_c_part[i][2] ,dl1r_b[0], cut_val);
     
-    sprintf(text1,"c eff =0.2, fb = %0.3f",fbs[i]);
-    sprintf(text2,"cut = %0.2f",getCut(hist_dlrc[i][1]));
+    tmp = CountJets(dl1r_c_part[i][0] ,dl1r_b[0], -10);
+    lr = tmp/CountJets(dl1r_c_part[i][0] ,dl1r_b[0], cut_val);
+    
+    sprintf(text1,"c eff =%0.2f, fb = %0.3f",target_c_efficiency,fbs[i]);
+    sprintf(text2,"cut = %0.2f",cut_val);
+    sprintf(text3,"rej b=%0.2f , l=%0.2f",br,lr );
+    cout <<text3<< endl;
     sprintf(canvas_name,"c_fbs_%d",i);
-    canv[i] = new TCanvas(canvas_name, "", 800, 600);
+    cout <<canvas_name<< endl;
+    canv[i] = new TCanvas(canvas_name, canvas_name, 800, 600);
     canv[i]->SetLogy();
     legend[i] = new TLegend(0.67,0.6,0.9,0.9);
     legend[i]->SetTextFont(42);legend[i]->SetFillColor(0);  legend[i]->SetBorderSize(0); legend[i]->SetFillStyle(0);  legend[i]->SetTextSize(0.05);
-    cout <<getCut(hist_dlrc[0][1])<< endl;
 
     for(int k=0; k<(int)typeflav.size();k++){             
       if(k==0){
+	hist_dlrc[i][k]->SetMaximum(hist_dlrc[i][0]->GetMaximum()*10);
 	hist_dlrc[i][k]->Draw("hist");
       }
       hist_dlrc[i][k]->Draw("histsame");
       hist_dlrc[i][k]->SetLineColor(color_sample[k]);
       legend[i]->AddEntry(hist_dlrc[i][k],(typeflav[k]).c_str(),"l");
     }
+    ll[i] = new TLine(cut_val, 0, cut_val, hist_dlrc[i][1]->GetMaximum());
+    ll[i]->SetLineStyle(2);
+    ll[i]->Draw("same");
+
     ATLASLabel(0.18,0.87,atl_lable,1,0.05);
     latex2.DrawLatex(0.18, 0.8, text1);
-    latex2.DrawLatex(0.18, 0.73, text2);
+    latex2.DrawLatex(0.18, 0.75, text2);
+    latex2.DrawLatex(0.18, 0.7, text3);
     legend[i]->Draw("same");
+
+    //cout << "fbs= "<< fbs[i]<<" ;cut_val="<<cut_val << " br = "<<br<< ", lr ="<<lr <<endl;
+    br =-99;lr=-99;
+
+    sprintf(o_name,"Plots/ctag_%0.0f/ttbar_dl1r_fb_%0.2f.pdf",target_c_efficiency*100,fbs[i]);
+    cout <<o_name<< endl;
+    canv[i]->Print(o_name);                                                                                                                                                                                              
+
+
   }
 
+  //*
   cout << CountJets(dl1r_c[0] ,dl1r_b[0], getCut(hist_dlrc[0][1]))<<endl;
 
   cout << CountJets(dl1r_c_part[0][1] ,dl1r_b[0], getCut(hist_dlrc[0][1]))<<endl;
@@ -163,7 +193,7 @@ void ctag(){
   cout << CountJets(dl1r_c_part[0][2] ,dl1r_b[0], -10)<<endl;
   cout << CountJets(dl1r_c_part[0][0] ,dl1r_b[0], getCut(hist_dlrc[0][1]))<<endl;
   cout << CountJets(dl1r_c_part[0][0] ,dl1r_b[0], -10)<<endl;
-    
+  //*/
 }
 
 void ATLASLabel(Double_t x,Double_t y,const char* text,Color_t color, Double_t tsize)
